cf08bf9ea157d77bf4caa8fc49b1cffdbafabc87
Chech3 <joseangelgarces123@gmail.com>
2024-03-01 01:08:23 -0400

    agregada foto

--- src/app/components/SocialMedia.jsx
+++ src/app/components/SocialMedia.jsx
@@ -6,15 +6,15 @@ import { FaGithub } from "react-icons/fa";
 
 function SocialMedia() {
   return (
-    <div>
-      <Link href="#">
+    <div className="py-6 flex">
+      <Link className="pr-2 text-2xl hover:text-sky-600 ease-out duration-300" target="_blank" href="https://www.facebook.com/joseangel.garces.14/">
         <FaFacebook />
       </Link>
-      <Link href="#">
-        <FaInstagramSquare />
+      <Link className="pr-2 text-2xl hover:text-violet-500 ease-out duration-300" target="_blank" href="#">
+        <FaInstagramSquare className="" />
       </Link>
-      <Link href="#">
-        <FaGithub />
+      <Link className="pr-2 text-2xl hover:text-slate-400 dark:hover:text-slate-800 ease-out duration-300" target="_blank" href="https://github.com/Chech3">
+        <FaGithub className="" />
       </Link>
     </div>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,36 @@
 import SocialMedia from "./components/SocialMedia";
+
 export default function Home() {
   return (
     <>
       <main>
-        <h1>Hola soy </h1>
-        <p>José Angel Garces Higuera</p>
-        <p>Me gusta mucho el Diseño Web</p>
-        <p>
-        Ingeniero en Sistemas, especializado en el desarrollo de aplicaciones web. 
-        Actualmente estoy en busca de nuevas oportunidades laborales.
-        </p>
-      <SocialMedia />
-        
+        <section className="my-0 flex flex-col lg:flex-row lg:h-[600px]">
+          <div className="p-10 lg:w-[80%] lg:h-[500px] lg:self-center">
+            <h1 className="text-sky-600 dark:text-sky-300 text-2xl font-bold tracking-widest leading-10">
+              Hola soy{" "}
+            </h1>
+            <p className="text-3xl py-2 font-bold text-slate-600 dark:text-slate-300">
+              José Angel Garces Higuera
+            </p>
+            <p className="text-3xl font-bold text-slate-500 dark:text-slate-300">
+              Me gusta mucho el Diseño Web
+            </p>
+            <p className="mt-5 text-lg">
+              Ingeniero en Sistemas, especializado en el desarrollo de
+              aplicaciones web. Actualmente estoy en busca de nuevas
+              oportunidades laborales.
+            </p>
+            <SocialMedia />
+            <div>
+              <button className="border-2 rounded border-sky-500 p-4 w-[250px] text-lg tracking-widest hover:bg-sky-500 hover:dark:text-slate-900 ease-out duration-300">
+                Contrátame
+              </button>
+            </div>
+          </div>
+          <div className="my-10 w-full ">
+            <div className="w-[300px] h-[320px] m-auto bg-[url('../../public/img/profile.png')] lg:bg-[url('../../public/img/profile.png')] bg-cover rounded-lg lg:w-[100%] lg:h-[100%] lg:bg-center lg:bg-no-repeat lg:bg-[length:450px_576px]"></div>
+          </div>
+        </section>
       </main>
     </>
   );
